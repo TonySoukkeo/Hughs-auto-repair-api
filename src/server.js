94e44eb17a03ab2fb46b8111ac015def1b09c6ac
Tony <tonysoukkeodesign@gmail.com>
2020-03-12 15:31:35 -0600

    cors

--- src/server.js
+++ src/server.js
@@ -70,6 +70,7 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 // Set headers
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Content-Type, Authorization, , X-Requested-With, Origin, Accept"
