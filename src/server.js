8e50f80773fc9c8cfe546e5c383d5b6865b899a6
Tony <tonysoukkeodesign@gmail.com>
2020-03-13 14:01:16 -0600

    cors

--- src/server.js
+++ src/server.js
@@ -76,7 +76,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, , X-Requested-With, Origin, Accept"
+    "Content-Type, Authorization, , X-Requested-With, Origin, Accept, Access-Control-Allow-Origin"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
